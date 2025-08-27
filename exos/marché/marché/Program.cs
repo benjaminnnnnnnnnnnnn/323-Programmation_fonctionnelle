297e8abd1300e6e5599c9a3e9e5585225b747a73
benjaminnnnnnnnnnnnn <167529563+benjaminnnnnnnnnnnnn@users.noreply.github.com>
2025-08-27 11:11:19 +0200

    marché LINQ 🥳🥳🥳🥳🥳🥳🥳

---
+++ exos/marché/marché/Program.cs
@@ -0,0 +1,88 @@
+﻿using IronXL;
+using System;
+using System.Linq;
+
+namespace marché
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkBook workBook = WorkBook.Load("C:\\Users\\px25twk\\Documents\\github\\323-Programmation_fonctionnelle\\exos\\marché\\Placedumarché.xlsx");
+            WorkSheet workSheet = workBook.WorkSheets[0];
+
+            int numPeches = 0;
+            string standpastèque = "";
+            int numpastèque = 0;
+            string nom = "";
+
+            List<marchand> marchands = new List<marchand>();
+
+
+            foreach (var cell in workSheet["A2:A75"])
+            {
+                marchands.Add(new marchand(workSheet[$"B{cell.Address.FirstRow + 1}"].First().Text,
+                                            cell.Text, 
+                                            workSheet[$"C{cell.Address.FirstRow + 1}"].First().Text,
+                                            Convert.ToInt32(workSheet[$"D{cell.Address.FirstRow + 1}"].First().Text)));
+            }
+
+
+
+            //LINQ 
+            int Marchandpeche = (from cell in marchands
+                            where cell.produit == "Pêches"
+                            select 1).Count();
+
+              var marchandpasteque = (from cell in marchands
+                                          where cell.produit == "Pastèques" && cell.num == marchands.Max(x => x.num)
+                                          select cell).First();
+            
+
+            foreach (var cell in workSheet["C2:C75"])
+            {
+
+                if (cell.Text == "Pêches")
+                    numPeches++;
+
+
+
+                if (cell.Text == "Pastèques" && numpastèque < Convert.ToInt32(workSheet[$"D{cell.Address.FirstRow + 1}"].First().Text))
+                {
+                    standpastèque = workSheet[$"A{cell.Address.FirstRow + 1}"].First().Text;
+                    numpastèque = Convert.ToInt32(workSheet[$"D{cell.Address.FirstRow + 1}"].First().Text);
+                    nom = workSheet[$"B{cell.Address.FirstRow + 1}"].First().Text;
+                }
+            }
+
+
+
+
+            Console.WriteLine($"Il y {Marchandpeche} vendeurs de pêches");
+            Console.WriteLine($"C'est {marchandpasteque.nom} qui a le plus de pastèques (stand {marchandpasteque.place}, {marchandpasteque.num} pièces)");
+
+
+
+            Console.ReadKey(true);
+
+        }
+
+        public class marchand
+        {
+
+
+            public marchand(string nom, string place, string produit, int num)
+            {
+                this.nom = nom;
+                this.place = place;
+                this.produit = produit;
+                this.num = num;
+            }
+
+            public string nom {  get; set; }
+            public string place {  get; set; }
+            public string produit { get; set; }
+            public int num {  get; set; }
+        }
+    }
+}
---
+++ sequences/NCDU/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NCDU
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string directory = "C:\\";
+
+            var fileList = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            var fileQuery = from file in fileList
+                            let fileLen = new FileInfo(file).Length
+                            where fileLen > 0
+                            select fileLen;
+
+            // Cache the results to avoid multiple trips to the file system.
+            long[] fileLengths = fileQuery.ToArray();
+
+            // Return the size of the largest file
+            long largestFile = fileLengths.Max();
+
+            // Return the total number of bytes in all the files under the specified folder.
+            long totalBytes = fileLengths.Sum();
+
+            Console.WriteLine($"There are {totalBytes} bytes in {fileList.Count()} files under {directory}");
+            Console.WriteLine($"The largest file is {largestFile} bytes.");
+        }
+    }
+}
